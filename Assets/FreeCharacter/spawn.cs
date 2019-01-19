883e07f91c107d3dbedf044254dba7f9acbd1b6a
legendsanjana <hoodaqwerty@gmail.com>
2019-01-19 23:43:39 +0530

    hey

---
+++ Assets/FreeCharacter/spawn.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+
+public class spawn : MonoBehaviour
+{
+    public GameObject nagent;
+    public GameObject goalObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("SpawnAgent", 2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject na = (GameObject)Instantiate(nagent, this.transform.position, Quaternion.identity);
+        na.GetComponent<walkTo>().goal = goalObject.transform;
+        Invoke("spawnAgent", Random.Range(2, 5));
+    }
+}
---
+++ Assets/FreeCharacter/walkTo.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+public class walkTo : MonoBehaviour {
+    public Transform goal;
+	// Use this for initialization
+	void Start () {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = goal.position;
+	}
+	
+
+}
